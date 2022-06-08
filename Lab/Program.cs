75971b9ca64674754c47d47a85058ddbf98cbee6
Abu Hasnat <hasnat.tushar@gmail.com>
2022-06-07 20:05:41 -0500

    Initial Lab3

--- Lab/Program.cs
+++ Lab/Program.cs
@@ -1,2 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿bool IsPalindrome(string testString)
+{
+
+    String newStr = testString;
+    for(int i = 0; i < testString.Length; i++)
+    {
+        for(int j = newStr.Length - 1; j >= 0; j--)
+        {
+            if(newStr[j] == testString[i])
+            {
+                return true;
+            }
+
+
+            //if(testString[j] == testString[i])
+            //{
+            //    return true;
+            //}
+        }
+    }
+    return false;
+}
+
+//Console.WriteLine(IsPalindrome("nap"));
+
+
+
+
+char[] DuplicateCharacters(string testString)
+{
+    char[] result = new char[testString.Length];
+    for(int i = 0; i < testString.Length; i++)
+    {
+        for(int j = i+1; j < testString.Length -1; j++)
+        {
+            if(testString[j] == testString[i])
+            {
+                result[i] = testString[i];
+                Array.Resize(ref result, result.Length - 1);
+               
+            }
+        }
+    }
+
+    return result;
+}
+
+
+//String myStr = "this is a string";
+//Console.WriteLine(DuplicateCharacters(myStr));
