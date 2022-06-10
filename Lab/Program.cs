2608f5ae73748d49ba341264f9a91c3cf0ce9391
Abu Hasnat <hasnat.tushar@gmail.com>
2022-06-09 20:53:00 -0500

    Final Lab3

--- Lab/Program.cs
+++ Lab/Program.cs
@@ -1,50 +1,53 @@
-﻿bool IsPalindrome(string testString)
+﻿using System.Text;
+
+bool IsPalindrome(string testString)
 {
-
-    String newStr = testString;
-    for(int i = 0; i < testString.Length; i++)
+    StringBuilder sb = new StringBuilder();
+    char[] myChars = testString.ToCharArray();
+    char[] reverseChars = new char[myChars.Length];
+    for(int i = myChars.Length - 1; i >= 0; i++)
     {
-        for(int j = newStr.Length - 1; j >= 0; j--)
+        reverseChars[i] = myChars[i];
+        if (myChars[i] == reverseChars[i])
         {
-            if(newStr[j] == testString[i])
-            {
-                return true;
-            }
-
-
-            //if(testString[j] == testString[i])
-            //{
-            //    return true;
-            //}
+            return true;
         }
     }
+
+
     return false;
 }
 
-//Console.WriteLine(IsPalindrome("nap"));
+
+
+
 
 
 
 
 char[] DuplicateCharacters(string testString)
 {
-    char[] result = new char[testString.Length];
-    for(int i = 0; i < testString.Length; i++)
+
+    char[] myChars = new char[testString.Length];
+    char[] results = new char[testString.Length];
+
+    for(int i = 0; i < myChars.Length; i++)
     {
-        for(int j = i+1; j < testString.Length -1; j++)
+        for(int j = i+1; j < myChars.Length; i++)
         {
-            if(testString[j] == testString[i])
+            if (myChars[i] == myChars[j])
             {
-                result[i] = testString[i];
-                Array.Resize(ref result, result.Length - 1);
-               
+                
+                Array.Resize(ref results, results.Length - 1);
+                break;
+            }
+            else
+            {
+                results[i] += myChars[i];
             }
         }
     }
-
-    return result;
+    return results;
 }
 
 
-//String myStr = "this is a string";
-//Console.WriteLine(DuplicateCharacters(myStr));
