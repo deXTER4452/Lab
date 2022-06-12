c8a534ed9dc351007ea138627dcee1d5402f7fa5
Abu Hasnat <hasnat.tushar@gmail.com>
2022-06-11 20:58:09 -0500

    Incomplete

--- Lab/Program.cs
+++ Lab/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿List<int> MaxNumbersInLists(List<List<int>> newList)
+{
+    List<int> myList = new List<int>();
+    myList = newList.Max();
+
+    return myList;
+};
+
+String HighestGrade(List<List<int>> newList)
+{
+    String myString = String.Empty;
+
+
+    return myString;
+}
+
+
+List<int> OrderByLooping(List<int> newList)
+{
+    
+    for(int i = 0; i < newList.Count; i++)
+    {
+        if(newList[i] > newList[i + 1])
+        {
+            int newInt = newList[i];
+            newList[i] = newList[i+1];
+            newList[i+1] = newInt;
+        }
+    }
+    return newList;
+}
